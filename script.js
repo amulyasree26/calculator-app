649e25faf9c4011c0bbaabfb02f6488d5a814ab3
Amulyasree <amulyasreemanepalli338@gmail.com>
2026-06-17 15:38:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-function add() {
+function calculate(operator) {
     let a = Number(document.getElementById("num1").value);
     let b = Number(document.getElementById("num2").value);
+    let result;
+
+    switch(operator) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = b !== 0 ? a / b : "Cannot divide by zero";
+            break;
+    }
 
     document.getElementById("result").innerText =
-        "Result: " + (a + b);
+        "Result: " + result;
 }
